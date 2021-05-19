7163518d2b1f5cbd279814fd06b092e7bae4182e
Aldrey Laudit <rorounni.aldrey.laudit@gmail.com>
2021-05-19 09:45:21 +0800

    Version 1 by Laudit

---
+++ Yeet.py
@@ -0,0 +1,32 @@
+print("""
+Name: Laudit, Rorounni Aldrey D.
+Course and Section: BSIT 2-E
+Contact number: 09087349980
+Email Address: rorounni.aldrey.laudit@gmail.com""")
+print("""
+   |   | 
+ X | X | O
+---+---+---
+ O | O |
+---+---+---  
+ X |   |   
+   |   |   
+""")
+print("""
+BBBBB       CCCC    DDDDD  
+B    B    CC        D    D
+BBBBB     C         D    D
+B    B    CC        D    D
+BBBBB       CCCC    DDDDD
+""")
+print("""
+     XXXXXXXX
+    X        X
+   X  _    _  X
+  X   O    O   X
+ X       |      X
+ X      _|      X
+  X            X
+   X \______/ X
+    XXXXXXXXXX
+""")
